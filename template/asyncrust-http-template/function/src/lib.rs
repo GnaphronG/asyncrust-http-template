1ab5862d5dee9b0c076d8b737ade34b4cb5b6966
Gnaphron <gnaphron@protonmail.com>
2020-12-27 16:30:42 -0800

    some necessary updates

--- template/asyncrust-http-template/function/src/lib.rs
+++ template/asyncrust-http-template/function/src/lib.rs
@@ -1,6 +1,6 @@
 use hyper::{Body, Request, Response};
 
-const PHRASE: &str = "Hello, World!";
+const PHRASE: &str = "Hello, Rust ⚙";
 
 pub async fn handle(_req: Request<Body>) -> Response<Body> {
     Response::new(Body::from(PHRASE))
